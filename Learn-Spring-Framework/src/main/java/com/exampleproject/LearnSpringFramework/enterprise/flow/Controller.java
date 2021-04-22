b27ad2f7314311c60eff48b6e72bafa0982f7cc2
Larissa Pinto <larissapinto98@student.sfit.ac.in>
2021-04-22 12:02:38 +0530

    Delete Learn-Spring-Framework directory

--- Learn-Spring-Framework/src/main/java/com/exampleproject/LearnSpringFramework/enterprise/flow/Controller.java
+++
@@ -1,8 +0,0 @@
-package com.exampleproject.LearnSpringFramework.enterprise.flow;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class Controller {
-	
-}
